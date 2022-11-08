a8f26c8ad59e84bcb2f48ea875ad5ae4d78ae464
Svartov <100573588+Svartov@users.noreply.github.com>
2022-11-08 17:10:12 +0200

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,35 @@
+package sample;
+
+import java.util.Scanner;
+
+
+public class Program {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Student student = new Student("Davyd", "Visko",Gender.man, 0, "231");
+        Group group = new Group();
+
+        getGroup(student, group);
+
+
+        System.out.println(student.getName());
+        System.out.println(student.getLastName());
+
+
+    }
+
+    public static void getGroup(Student student, Group group){
+        try {
+            group.addStudent(student);
+            System.out.println("Complete");
+            System.out.println(group);
+        }catch (GroupOverflowException  e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ Student.java
@@ -0,0 +1,45 @@
+package sample;
+
+public class Student extends Human{
+
+    private int id;
+    private String groupName;
+
+    public Student(String name, String lastName, Gender gender, int id, String groupName) {
+        super(name, lastName, gender);
+        this.id = id;
+        this.groupName = groupName;
+    }
+
+    public Student(String name, String lastName, Gender gender) {
+        super(name, lastName, gender);
+    }
+
+    //  Get
+
+    public int getId(){
+        return  id;
+    }
+
+    public String getGroupName(){
+        return groupName;
+    }
+
+    // Set
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setGroupName(String groupName){
+        this.groupName = groupName;
+    }
+
+    // ToString
+
+    @Override
+    public String toString(){
+        return "[" + "ID = " + id + " GroupName = " + groupName + "]";
+    }
+
+}
